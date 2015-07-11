58de59b2cca4fb72fc63f3f24afffc2cd17b34a2
Freika <iamfrey@gmail.com>
2015-07-12 01:27:55 +0600

    Add subdomains to accounts.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710154700) do
+ActiveRecord::Schema.define(version: 20150711191905) do
 
   create_table "subscribem_accounts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "owner_id"
+    t.string   "subdomain"
   end
+
+  add_index "subscribem_accounts", ["subdomain"], name: "index_subscribem_accounts_on_subdomain"
 
   create_table "subscribem_users", force: :cascade do |t|
     t.string   "email"
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -4,6 +4,7 @@ feature 'Accounts' do
     visit subscribem.root_path
     click_link 'Account Sign Up'
     fill_in 'Name', with: 'Test'
+    fill_in 'Subdomain', with: 'test'
     fill_in 'Email', with: 'test@test.com'
     fill_in 'Password', with: '00000000'
     fill_in 'Password confirmation', with: '00000000'
@@ -12,5 +13,6 @@ feature 'Accounts' do
 
     expect(page).to have_content(success_message)
     expect(page).to have_content("Signed in as test@test.com")
+    expect(page.current_url).to eq('http://test.example.com/subscribem/')
   end
 end
