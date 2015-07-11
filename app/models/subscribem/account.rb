5fabc2ab25a825e7a18f3fcb7f698231f625d27d
Freika <iamfrey@gmail.com>
2015-07-12 01:49:06 +0600

    Forbid some certains subdomains.

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -1,7 +1,17 @@
 module Subscribem
   class Account < ActiveRecord::Base
+    EXCLUDED_SUBDOMAINS = %w(admin)
+
+    before_validation { self.subdomain = subdomain.to_s.downcase }
+
     validates :subdomain, presence: true, uniqueness: true
+    validates_exclusion_of :subdomain, in: EXCLUDED_SUBDOMAINS,
+      message: 'is not allowed. Please choose another subdomain.'
+    validates_format_of :subdomain, with: /\A[\w\-]+\Z/i,
+      message: 'is not allowed. Please choose another subdomain.'
+
     belongs_to :owner, class_name: 'Subscribem::User'
     accepts_nested_attributes_for :owner
+
   end
 end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 feature 'Accounts' do
   scenario 'creating an account' do
-    do_register_movements
+    do_register_movements('Test', 'test', 'test@test.com')
     success_message = 'Your account has been successfully created.'
 
     expect(page).to have_content(success_message)
@@ -11,19 +11,35 @@ feature 'Accounts' do
 
   scenario 'ensure subdomain uniqueness' do
     Subscribem::Account.create!(subdomain: 'test', name: 'Test')
-    do_register_movements
+    do_register_movements('Test', 'test', 'test@test.com')
 
     expect(page.current_url).to eq('http://www.example.com/subscribem/accounts')
     expect(page).to have_content('Sorry, your account could not be created.')
     expect(page).to have_content('Subdomain has already been taken')
   end
 
-  def do_register_movements
+  scenario 'subdomain with restricted name' do
+    do_register_movements('Test', 'admin', 'test@test.com')
+
+    expect(page.current_url).to eq('http://www.example.com/subscribem/accounts')
+    expect(page).to have_content('Sorry, your account could not be created.')
+    expect(page).to have_content('Subdomain is not allowed. Please choose another subdomain.')
+  end
+
+  scenario 'subdomain with restricted name' do
+    do_register_movements('Test', '<admin>', 'test@test.com')
+
+    expect(page.current_url).to eq('http://www.example.com/subscribem/accounts')
+    expect(page).to have_content('Sorry, your account could not be created.')
+    expect(page).to have_content('Subdomain is not allowed. Please choose another subdomain.')
+  end
+
+  def do_register_movements(name, subdomain, email)
     visit subscribem.root_path
     click_link 'Account Sign Up'
-    fill_in 'Name', with: 'Test'
-    fill_in 'Subdomain', with: 'test'
-    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Name', with: name
+    fill_in 'Subdomain', with: subdomain
+    fill_in 'Email', with: email
     fill_in 'Password', with: '00000000'
     fill_in 'Password confirmation', with: '00000000'
     click_button 'Create Account'
