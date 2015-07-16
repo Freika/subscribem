b6b82599cee8a89ea5fa1a94b4feefaa0a2d3d6a
Freika <iamfrey@gmail.com>
2015-07-16 18:46:32 +0600

    Restrict signing in with invalid data.

--- app/controllers/subscribem/account/sessions_controller.rb
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -10,6 +10,10 @@ module Subscribem
       if env['warden'].authenticate(scope: :user)
         flash[:notice] = 'You are now signed in.'
         redirect_to root_path
+      else
+        @user = User.new
+        flash[:error] = 'Invalid email or password.'
+        render :new
       end
     end
   end
--- spec/features/accounts/sign_in_spec.rb
+++ spec/features/accounts/sign_in_spec.rb
@@ -19,5 +19,29 @@ feature 'User sign in' do
       expect(page).to have_content('You are now signed in.')
       expect(page.current_url).to eq(root_url)
     end
+
+    scenario 'attempts sign in with invalid password and fails' do
+      visit subscribem.root_url(subdomain: account.subdomain)
+      expect(page.current_url).to eq(sign_in_url)
+      expect(page).to have_content('Please sign in.')
+      fill_in 'Email', with: account.owner.email
+      fill_in 'Password', with: 'drowssap'
+      click_on 'Sign in'
+
+      expect(page).to have_content('Invalid email or password')
+      expect(page.current_url).to eq(sign_in_url)
+    end
+
+    scenario 'attempts sign in with invalid email and fails' do
+      visit subscribem.root_url(subdomain: account.subdomain)
+      expect(page.current_url).to eq(sign_in_url)
+      expect(page).to have_content('Please sign in.')
+      fill_in 'Email', with: 'em@ail.com'
+      fill_in 'Password', with: 'password'
+      click_on 'Sign in'
+
+      expect(page).to have_content('Invalid email or password')
+      expect(page.current_url).to eq(sign_in_url)
+    end
   end
 end
